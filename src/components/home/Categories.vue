c9c74688b3a52e9b7c3d54ab578ccf10bf912c11
luthfi Amrullah <mluthfiamrullah@gmail.com>
2022-11-17 09:11:05 +0700

    create items component

--- src/components/home/Categories.vue
+++ src/components/home/Categories.vue
@@ -2,15 +2,45 @@
   <div class="container px-4 mx-auto my-16 md:px-12">
     <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
-      <CategoriesCard title="Mobile UI kit" count="732" image="/src/assets/img/categories-1.jpg"/>
-      <CategoriesCard title="Fonts" count="657" image="/src/assets/img/categories-2.jpg"/>
-      <CategoriesCard title="Icon Set" count="84559" image="/src/assets/img/categories-3.jpg"/>
-      <CategoriesCard title="Website UI Kit" count="4500" image="/src/assets/img/categories-4.jpg"/>
+      <CategoriesCard
+        v-for="category in categories"
+        :key="category"
+        :title="category.title"
+        :count="category.count"
+        :image="category.image"
+      />
     </div>
   </div>
 </template>
 <script setup>
-import { RouterLink } from 'vue-router'
-import CategoriesCard from '../CategoriesCard.vue'
+import { RouterLink } from "vue-router";
+import CategoriesCard from "../CategoriesCard.vue";
+import { ref } from "vue";
 
+const categories = ref([
+  {
+    id: 1,
+    title: "Mobile UI Kit",
+    count: 732,
+    image: "/src/assets/img/categories-1.jpg",
+  },
+  {
+    id: 2,
+    title: "Fonts",
+    count: 732,
+    image: "/src/assets/img/categories-2.jpg",
+  },
+  {
+    id: 3,
+    title: "Icon Set",
+    count: 732,
+    image: "/src/assets/img/categories-3.jpg",
+  },
+  {
+    id: 4,
+    title: "Website UI Kit",
+    count: 732,
+    image: "/src/assets/img/categories-4.jpg",
+  },
+]);
 </script>
---
+++ src/components/home/Items.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container px-4 mx-auto my-16 md:px-12">
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+      <ItemsCard v-for="item in items" :key="item" :title="item.title" :image="item.image"/>
+    </div>
+  </div>
+</template>
+<script setup>
+import ItemsCard from '../ItemsCard.vue'
+import { ref } from 'vue';
+
+const items = ref([
+  { id: 1, title: 'Mobile UI Kit', image: '/src/assets/img/items-1.jpg' },
+  { id: 1, title: 'Online Doctor Consultation', image: '/src/assets/img/items-2.jpg' },
+  { id: 1, title: 'Banking Crypto', image: '/src/assets/img/items-3.jpg' }
+])
+
+</script>
